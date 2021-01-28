1277eb8552675e7bc943e56fa7c644f37e44935d
Ye117 <q502461275@gmail.com>
2021-01-28 13:13:08 +0100

    Segunda prueba.

---
+++ GGJ2021/Assets/Scripts/Jugador.cs
@@ -0,0 +1,11 @@
+/*En este script vamos a poner todo lo relacionado al comportamiento del jugador,
+ * como los saltos, los movimientos,etc.*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+   
+}
+
---
+++ GGJ2021/Assets/Scripts/PruebaGitHub.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PruebaGitHub : MonoBehaviour
+{
+    //Adrian, podemos usar este script para probar de que efectivamente los cambios que tu hagas y los cambios 
+    // que yo haga se van a cambiar. 
+    //ESTE SCRIPT LO ELIMINAREMOS CUANDO ESTEMOS SEGURO DE QUE LOS CAMBIOS SE GUARDAN
+    private void Start()
+    {
+        //Una simple prueba
+        print(1 + 1);
+    }
+
+
+}
